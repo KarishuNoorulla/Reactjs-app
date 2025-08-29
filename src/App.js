3c6137c7407494badd94756de194e45d23061640
noorulla <noorullak007@gmail.com>
2025-08-29 12:23:34 +0530

    new page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import DisneyStyleClone from "./DisneyStyleClone"; // make sure the file name matches
 import Sprite from "./sprite"
+import Cartoon from "./CartoonHub"
 function App() {
   return (
     <div>
+      
       <DisneyStyleClone />
       <Sprite />
+      <Cartoon/>
     </div>
   );
 }
